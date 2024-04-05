98baf476457e519e0fd55e1f36d5f789504d414b
HollandBen-P2 <benjaminh62@kprschools.ca>
2024-04-05 11:02:43 -0400

    Day 6: wait we're so back?????????

--- databasing/main.py
+++ databasing/main.py
@@ -3,9 +3,7 @@
 #Student grades program v2.0 (database ver.)
 
 #to do:
-    #removing student is not working at the database command
-    #give feedback that the student and their grades were added
-    #work on commands listed as "NEED FIX"
+    #literally all of the "students" command line
 
 #database starter
 import database
@@ -84,7 +82,7 @@ def edit_student():
         if edit_student_choice == "add":
             print(border)
             print("Enter the new student's name.\nEnter 'Q' to return to the student editor menu.")
-            breaker = False
+            breaker = False 
             while True:
                 if breaker:
                     break
@@ -96,7 +94,7 @@ def edit_student():
                 else:
                     new_student_name.strip().capitalize()
                     print(f"Student '{new_student_name}' was accepted.")
-                    print("Enter the five assignment grades associated with this student. Use the format: xx, xx, xx, xx, xx\nEnter 'Q' to return to the student editor menu.")
+                    print(r"Enter the five assignment grades between 0 and 115 % associated with this student. Use the format: xx, xx, xx, xx, xx\nEnter 'Q' to return to the student editor menu.")
                     while True:
                         call_adding_items = input("> Enter input: ")
                         if call_adding_items.lower().strip() == "q":
@@ -114,13 +112,12 @@ def edit_student():
                                 database.insert_db(conn, "Students", ["name", "assign_1", "assign_2", "assign_3", "assign_4", "assign_5"], [new_student_name, round(float(new_grade_1), 2), round(float(new_grade_2), 2), round(float(new_grade_3), 2), round(float(new_grade_4), 2), round(float(new_grade_5), 2)])
                                 fetch()
                                 print(f"Student #{gradebook_list[-1][0]} {new_student_name}, with grades of {new_grade_1} %, {new_grade_2} %, {new_grade_3} %, {new_grade_4} %, and {new_grade_5} % was added to the gradebook.")
-                                breaker = True
+                                breaker = True #used to escape all of the loops kinda jank but wtv
                                 break
                             except ValueError or TypeError:
                                 print("Invalid input. Please try again.")
         
-        #FIX REMOVING (needs to update the IDs in the database, otherwise functions will get the wrong person)
-        #command for removing a student from the class list
+        #removing an entire student and their grades from the system
         elif edit_student_choice == "remove":
             fetch()
             print(border)
@@ -129,38 +126,18 @@ def edit_student():
                 remove_student = input("> Enter input: ")
                 if remove_student.lower().strip() == "q":
                     break
-                for sublist in gradebook_list:
-                    try:
-                        if sublist[0] == int(remove_student):
+                try:
+                    for sublist in gradebook_list: #checks every student in the class
+                        if sublist[0] == int(remove_student): #checks the id
                             print(f"Student '{sublist[1]}' was removed.")
                             #student_list.pop(int(remove_student) - 1)
                             database.delete_db(conn, "Students", "id", int(remove_student))
                             break
-                        else:
-                            print("Invalid student number. Please try again.")
-                    except ValueError:
-                        break
+                except ValueError:
+                    print("Invalid student number. Please try again.")
+                    break
                 break
                     
-                        
-                
-                
-                
-                
-                
-                try:
-                    if any(int(remove_student) in (match := sublist) for sublist in gradebook_list):
-                        print(f"Student '{gradebook_list[match.index(int(remove_student))][1]}' was removed.")
-                        #student_list.pop(int(remove_student) - 1)
-                        database.delete_db(conn, "Students", "id", int(remove_student))
-                        break
-                    elif remove_student.lower().strip() == "q":
-                        break
-                    else:
-                        print("Invalid student number. Please try again.")
-                except IndexError:
-                    print("Invalid student number. Please try again.")
-        
         #command for exiting the current menu and returning to the main menu
         elif edit_student_choice == "q":
             break
@@ -170,78 +147,56 @@ def edit_student():
 #editing one of the students from the class list
 def student_menu():
     while True:
+        fetch()
         global student_average, gradebook_list
         print(border)
         print("Class list:")
         for i in range(0, student_list_length):
-            print(i + 1, gradebook_list[i][1])
+            print(gradebook_list[i][0], gradebook_list[i][1])    
         print("Enter the student's number to access their profile.\nEnter 'Q' to return to the main menu.")
-        student_menu_choice = input("> Enter input: ")
         
         #checks to make sure that the student number that was entered is a valid part of the class
-        if student_menu_choice.isdigit() and int(student_menu_choice) <= student_list_length and int(student_menu_choice) >= 1:
-            while True:
-                print(border)
-                print(f"Student selected: {gradebook_list[int(student_menu_choice) - 1][1]}")
-                assignments_list = len(gradebook_list[int(student_menu_choice) - 1])
-                print("The recorded grades for this student are:")
-                for i in range(2, assignments_list):
-                    print(f"Assignment {i}: {(gradebook_list[int(student_menu_choice) - 1][i])} %")
-                print("Enter 'mean' to get this student's average, enter 'add' to add an assignment's grade to their progile, or enter 'remove' to remove an assignment.\nEnter 'Q' to return to student grade information menu.")
-                individual_choice = input("> Enter input: ")
-                
-                #gets the mean of the student's grades
-                if individual_choice.strip().lower() == "mean":
-                    student_adder = sum(gradebook_list[int(student_menu_choice) - 1][2:])
-                    try:
-                        student_average = student_adder / (assignments_list - 2)
-                    except ZeroDivisionError:
-                        student_average = 0
-                    print(border)
-                    print(f"The student's average is: {round(student_average, 2)} %.")
-                
-                #NEEDS FIX
-                #command to add a new assignment to the student's grades (in percent)
-                elif individual_choice.strip().lower() == "add":
-                    print(border)
-                    print(r"Enter the new percentage grade to be added. Grades between 0 % and 115 % are accepted.")
-                    new_grade = input("> Enter input: ")
-                    try:
-                        if float(new_grade) >= 0 and float(new_grade) <= 115:
-                            student_list[int(student_menu_choice) - 1].append(round(float(new_grade), 2))
-                        else:
-                            print("Invalid grade. Please try again.")
-                    except ValueError:
-                        print("Invalid grade. Please try again.")
-                
-                #NEEDS FIX
-                #command to remove an assignment from the student's grades
-                elif individual_choice.strip().lower() == "remove":
-                    print(border)
-                    print("Enter the assignment number to be removed.")
-                    delete_grade = input("> Enter input: ")
-                    
-                    #checks that the grade does exist in the assignments list
-                    if delete_grade.isdecimal() and int(delete_grade) >= 1 and int(delete_grade) < assignments_list:
-                        print(f"Grade '{student_list[int(student_menu_choice) - 1][int(delete_grade)]}' was removed")
-                        del student_list[int(student_menu_choice) - 1][int(delete_grade)]
-                    else:
-                        print("Invalid choice. Please try again.")
-                
-                #command for exiting the current menu and returning to the previous menu
-                elif individual_choice.strip().lower() == "q":
-                    break
-                else:
-                    print("Invalid choice. Please try again.")
         
-        #command for exiting the current menu and returning to the main menu
-        elif student_menu_choice == "q":
-            break
-        else:
-            print("Invalid choice. Please try again.")
+        while True:
+            student_menu_choice = input("> Enter input: ")
+            if student_menu_choice.lower().strip() == "q":
+                break
+            try:
+                for sublist in gradebook_list: #checks every student in the class
+                    if sublist[0] == int(student_menu_choice): #checks the id
+                        print(border)
+                        print(f"Student selected: {sublist[1]}")
+                        print("The recorded grades for this student are:")
+                        for i in range(2, 7):
+                            print(f"Assignment {i-1}: {sublist[i]} %")
+                        print("Enter 'mean' to get this student's average, or enter 'update' to alter one of the assignment grades.\nEnter 'Q' to return to student grade information menu.")
+                        individual_choice = input("> Enter input: ")
+                        
+                        if individual_choice.strip().lower() == "mean":
+                            student_adder = sum(sublist[2:])
+                            try:
+                                student_average = student_adder / 5
+                            except ZeroDivisionError:
+                                student_average = 0
+                            print(border)
+                            print(f"The student's average is: {round(student_average, 2)} %.")
+                
+                        elif individual_choice.strip().lower() == "update":
+                            print("Enter the assignment number that should be changed.\nEnter 'Q' to return to student grade information menu.")
+                            updater = input("> Enter input: ")
+                            if updater.lower().strip() == "q":
+                                break
+                            elif int(updater) >= 1 and int(updater) <= 5:
+                                pass
+                            else:
+                                print("Invalid assignment number. Please try again.")
+                break
+                            
+                            
+            except ValueError:
+                print("Invalid student number. Please try again.")
+                break
 
 
 #loop that actually runs the program constantly            
 main_menu()
-
-#STUDENTS > [X] (BROKEN)
--- databasing/test.py
+++ databasing/test.py
@@ -9,3 +9,21 @@ database.update_db(conn, "Students", ["first_name = 'Mester'", "age = '90'"], "i
 
 result = database.select_db(conn, "Students").fetchall()
 print(result)
+
+                
+                
+                
+                
+                
+                try:
+                    if any(int(remove_student) in (match := sublist) for sublist in gradebook_list):
+                        print(f"Student '{gradebook_list[match.index(int(remove_student))][1]}' was removed.")
+                        #student_list.pop(int(remove_student) - 1)
+                        database.delete_db(conn, "Students", "id", int(remove_student))
+                        break
+                    elif remove_student.lower().strip() == "q":
+                        break
+                    else:
+                        print("Invalid student number. Please try again.")
+                except IndexError:
+                    print("Invalid student number. Please try again.")
