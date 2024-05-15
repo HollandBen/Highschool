b178053efaff7d22cf4cf134184dd2a1d1ed0440
BenHolland-P2 <benjaminh62@kprschools.ca>
2024-05-15 11:40:16 -0400

    Day 3ish: idk work on it at home

--- maze_2/interface/game.py
+++ maze_2/interface/game.py
@@ -6,17 +6,21 @@ import pygame,sys,objects.movable,objects.images
 
 def output(WINDOW_WIDTH, WINDOW_HEIGHT, fpsClock, fps, window):
     #Setup of Starting objects
-    bg = objects.images.still(0,0,WINDOW_WIDTH,WINDOW_HEIGHT,'maze_2/images/background.png')
-    player = objects.movable.movable(25,25,45,30,1.25,'maze_2/images/bumble.jpg')      #feeding in custom object
+    bg = objects.images.still(0,0,WINDOW_WIDTH,WINDOW_HEIGHT,'maze_2/images/pixilart-drawing.png')
+    player = objects.movable.movable(90,50,45,30,1.25,'maze_2/images/bumble.jpg')      #feeding in custom object
 
     def display():
+        window.fill((255,255,255))
         bg.draw(window)
         player.draw(window) #new places character
 
     while True:
         player.move()
         display()
-
+        if pygame.sprite.collide_mask(player, bg):
+            player.move_back()
+            display()
+             
         for event in pygame.event.get():
         # if user  QUIT then the screen will close
             if event.type == pygame.QUIT:
--- maze_2/objects/movable.py
+++ maze_2/objects/movable.py
@@ -12,18 +12,23 @@ class movable(pygame.sprite.Sprite):
     
     def draw(self,window):
         window.blit(self.image,(self.rect.x, self.rect.y))
-    
+    def move_back(self):
+          #x-location + x-speed = new x-location
+        self.rect.x -= self.movex # make these act on self
+
+        #y-location + y-speed = new y-location
+        self.rect.y -= self.movey
     def move(self):
             #value name---pygame check if keys down---->Create a variable that is set to all the key values
         key_input = pygame.key.get_pressed()
 
         
         #var--------value name-----key Left---speed value--value name------key Right---speed value      
-        movex = (key_input[pygame.K_a] * -self.speed) + (key_input[pygame.K_d] * self.speed) #speed -> self.speed
-        movey = (key_input[pygame.K_w] * -self.speed) + (key_input[pygame.K_s] * self.speed)
+        self.movex = (key_input[pygame.K_a] * -self.speed) + (key_input[pygame.K_d] * self.speed) #speed -> self.speed
+        self.movey = (key_input[pygame.K_w] * -self.speed) + (key_input[pygame.K_s] * self.speed)
 
         #x-location + x-speed = new x-location
-        self.rect.x += movex # make these act on self
+        self.rect.x += self.movex # make these act on self
 
         #y-location + y-speed = new y-location
-        self.rect.y += movey
+        self.rect.y += self.movey
